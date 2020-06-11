fb60e05645f1a7e671f58493e41a7dab0d369bb3
Mayar <mayarmayar886@gmail.com>
2020-06-12 01:25:53 +0300

    mayar repair pageFiles

--- src/sample/Paths/PageFiles.java
+++ src/sample/Paths/PageFiles.java
@@ -18,36 +18,29 @@ public class PageFiles{
         JS=new JS(mainPath,"JS");
         media=new Media(mainPath,"Media");
     }
-
     public void saveIn(Path folder,String data,String nameOfFile) throws IOException {
-       String save= folder.getObjPath()+File.separator+nameOfFile;
+        String save= folder.getObjPath()+File.separator+nameOfFile;
         folder.writeFile(save,data);
         String temp=folder.readFile(save);
-        System.out.println(temp);
     }
-
     public Path getHTML() {
         return HTML;
     }
+
     public Path getMedia() {
         return media;
     }
+
     public Path getJS() {
         return JS;
     }
     public Path getCSS() {
         return CSS;
     }
-<<<<<<< HEAD
-
-
-    public void setURLS(String urls,String location,String page) throws IOException {
-=======
-    public void setURLS(String urls,String location,String nameOfFile) throws IOException {
->>>>>>> 067194cc0b072b48e07f6b55724b36a4c1423a00
+    public void setURLS(ArrayList<String> urls,String location,String namePage) throws IOException {
         File folder=new File(location);
         folder.mkdirs();
-        File saveURLS=new File(location+File.separator+nameOfFile);
+        File saveURLS=new File(location+File.separator+namePage);
         if (!(saveURLS.exists())){
             boolean safe= saveURLS.createNewFile();
             if (safe)
@@ -57,12 +50,12 @@ public class PageFiles{
         }
         FileOutputStream fos=new FileOutputStream(saveURLS,true);
         PrintWriter pw=new PrintWriter(fos);
-        pw.println(urls);
+        for (String temp:urls){
+            pw.println(temp);
+        }
         pw.close();
         fos.close();
     }
-
-
     public void deleteFirstLine(String file) throws IOException {
         File path = new File(file);
         Scanner scanner = new Scanner(path);
@@ -82,15 +75,8 @@ public class PageFiles{
 
         writer.close();
     }
-<<<<<<< HEAD
-
-
-    public String getOneURL(String page) throws IOException {
-        FileInputStream fis = new FileInputStream(page);
-=======
-    public String getOneURL(String nameOfFile) throws IOException {
-        FileInputStream fis = new FileInputStream(nameOfFile);
->>>>>>> 067194cc0b072b48e07f6b55724b36a4c1423a00
+    public String getOneURL(String pathOfFile) throws IOException {
+        FileInputStream fis = new FileInputStream(pathOfFile);
         InputStreamReader isr = new InputStreamReader(fis);
         BufferedReader br = new BufferedReader(isr);
         String oneLine = "";
@@ -103,11 +89,9 @@ public class PageFiles{
         pw.println(oneLine);
         pw.close();
         fos.close();
-        deleteFirstLine(nameOfFile);
+        deleteFirstLine(pathOfFile);
         return oneLine;
     }
-
-
     public String getOneURL_From_Downloading() throws IOException {
         FileInputStream fis = new FileInputStream(DOWNLOADING_FILE);
         InputStreamReader isr = new InputStreamReader(fis);
@@ -119,9 +103,7 @@ public class PageFiles{
         fis.close();
         return oneLine;
     }
-
-
-    public void removeOneURL_FromDownloading() throws IOException {
+    public void  removeOneURL_FromDownloading() throws IOException {
         File path = new File(DOWNLOADING_FILE);
         Scanner scanner = new Scanner(path);
         ArrayList<String> coll = new ArrayList<String>();
@@ -140,30 +122,15 @@ public class PageFiles{
 
         writer.close();
     }
-<<<<<<< HEAD
 
-    public boolean isURL_InDownloading() throws IOException {
-
-            File file = new File(DOWNLOADING_FILE);
-            return !file.exists() || file.length() <= 0;
-
-=======
-    public synchronized boolean isURL_InDownloading() throws IOException {
+    public synchronized boolean  isURL_InDownloading()  throws IOException {
         File file = new File(DOWNLOADING_FILE);
         return !file.exists() || file.length() <= 0;
->>>>>>> 067194cc0b072b48e07f6b55724b36a4c1423a00
     }
 
 
-    public synchronized boolean isURL_InURL_Text(String nameOfFile){
-        File file=new File(nameOfFile);
+    public synchronized boolean isURL_InURL_Text(String textPath){
+        File file=new File(textPath);
         return !file.exists() || file.length() <= 0;
     }
-    public long sizeOfFileInKB(String file){
-        return file.length()/1024;
-    }
-    public long sizeOfFileInMB(String file){
-        return sizeOfFileInKB(file)/1024;
-    }
-
 }
